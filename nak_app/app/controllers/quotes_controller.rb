a14b22e4046bc0e7b94a59617f6a83fe2dbe0850
Nick Harvey <nolds9@yahoo.com>
2015-03-15 12:51:59 -0400

    Set up quotes app

---
+++ nak_app/app/controllers/quotes_controller.rb
@@ -0,0 +1,15 @@
+class HeatsController < ApplicationController
+
+  def index
+    if params[:query]
+      query = params[:query].gsub(' ', '+')
+      #response = HTTParty.get("https://api.edamam.com/search?q=#{query}")
+      response = HTTParty.get("https://api.edamam.com/search?q=#{query}&app_id=e1898b85&app_key=565e8de817c50623627e2e8b1107117d")
+      result = JSON.parse(response.body)
+      @result = result['hits']
+    else
+      @result = nil
+    end
+  end
+
+end
