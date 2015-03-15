6f9b180eddf527d26a71d83913d584f0cc90cdf8
Nick Harvey <nolds9@yahoo.com>
2015-03-15 13:45:14 -0400

    fixed controllers

--- nak_app/app/controllers/quotes_controller.rb
+++ nak_app/app/controllers/quotes_controller.rb
@@ -4,7 +4,7 @@ class QuotesController < ApplicationController
     if params[:query]
       query = params[:query].gsub(' ', '+')
       #response = HTTParty.get("https://api.edamam.com/search?q=#{query}")
-      response = HTTParty.get("https://api.edamam.com/search?q=#{query}&app_id=e1898b85&app_key=565e8de817c50623627e2e8b1107117d")
+      response = HTTParty.get("http://capitolwords.org/api/1/text.json?phrase=#{query}&apikey=83af50945e0c4f90861fb472e7d148e3")
       result = JSON.parse(response.body)
       @result = result['hits']
     else
